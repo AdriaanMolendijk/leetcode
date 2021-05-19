b87c38bf853bcc8363555c1a48deeb785248dff6
Adriaan Molendijk <a.l.molendijk@outlook.com>
2021-05-19 16:31:52 +0200

    Added ex 1221

---
+++ java/Exercise1221.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int counter = 0;
+        int splits = 0;
+        for (char c : s.toCharArray()) {
+            if (c == 'R') {
+                counter++;
+            } else {
+                counter--;
+            }
+            if (counter == 0) {
+                splits++;
+            }
+        }
+        return splits;
+    }
+}
